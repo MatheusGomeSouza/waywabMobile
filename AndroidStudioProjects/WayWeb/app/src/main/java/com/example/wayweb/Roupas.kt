a6be4fe4d9861b28b237dd656735398de458a6fe
MatheusGomeSouza <matheus.gsouza@aluno.faculdadeimpacta.com.br>
2021-09-21 19:01:50 -0300

    final

--- AndroidStudioProjects/WayWeb/app/src/main/java/com/example/wayweb/Roupas.kt
+++ AndroidStudioProjects/WayWeb/app/src/main/java/com/example/wayweb/Roupas.kt
@@ -2,10 +2,16 @@ package com.example.wayweb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_roupas.*
 
 class Roupas : DebugActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_roupas)
+
+        comprar.setOnClickListener {
+            Toast.makeText(this, "Compra realizada com sucesso!", Toast.LENGTH_LONG).show()
+        }
     }
 }
