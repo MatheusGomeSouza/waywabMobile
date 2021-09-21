b08696f9d1105b3438967abcb0a6e41b39cfe1f6
MatheusGomeSouza <matheus.gsouza@aluno.faculdadeimpacta.com.br>
2021-09-21 18:04:46 -0300

    second

--- AndroidStudioProjects/WayWeb/app/src/main/java/com/example/wayweb/index.kt
+++ AndroidStudioProjects/WayWeb/app/src/main/java/com/example/wayweb/index.kt
@@ -2,10 +2,15 @@ package com.example.wayweb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_index.*
 
 class index : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_index)
+        val args = intent.extras
+
+        val nome_usuario = args?.getString("nome_usuario")
+        txt_usuario.text = nome_usuario
     }
 }
